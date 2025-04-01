30063d4c4e7040a6b13eaf2d07662e20d67daf60
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-04-01 07:20:39 +0200

    dataloader now works more in line with how it seems to be intended in tf-geometric

--- nd280mlreco/dataloader.py
+++ nd280mlreco/dataloader.py
@@ -1,33 +1,55 @@
 import typing
+from collections.abc import Callable
 import os
 
 import torch
-from torch_geometric.data import Dataset, Data
+from torch_geometric import data as tg_data
 
-class HATDataset(Dataset):
-    
-    def __init__(self, filenames:list[str], processed_file_path:str):
+class HATDataset(tg_data.Dataset):
+
+    def __init__(
+            self,
+            root:str,
+            raw_filenames:list[str], 
+            processed_file_names:list[str],
+            transform = None,
+            pre_transform=None, 
+            pre_filter=None
+            ):
+
         # keep copies of the raw and processed file names
-        self._raw_filenames:list[str] = list(filenames)
-        self._processed_filenames:list[str] = []
-
-        self._processed_file_path:str = processed_file_path
-
+        self.raw_filenames:list[str] = raw_filenames
+        self.processed_filenames:list[str] = processed_file_names
+        
+        super().__init__(root = root, transform = transform, pre_transform = pre_transform, pre_filter = pre_filter)
+    
     @property
     def raw_file_names(self) -> list[str]:
-        return self._raw_filenames
+        return self.raw_filenames
 
     @property
     def processed_file_names(self) -> list[str]:
-        return self._processed_filenames
+        return self.processed_filenames
 
     def process(self) -> None:
-        raise NotImplementedError()
-        
+        idx = 0
+        for raw_path in self.raw_filenames:
+            # Read data from `raw_path`.
+            data = torch.load(raw_path)
+
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+
+            torch.save(data, os.path.join(self.root, "processed", self.processed_file_names[idx]))
+            idx += 1
+
     def len(self):
-        raise NotImplementedError()
-    
+        return len(self.processed_filenames)
+
     def get(self, idx):
-        data = torch.load(os.path.join(self._processed_file_path, f'data_HAT_{idx}.pt'))
+        data = torch.load(os.path.join(self.root, "processed", self.processed_file_names[idx]))
         return data
 
